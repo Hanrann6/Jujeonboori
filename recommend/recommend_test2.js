6f35427d96fd0f48ec9d115bc59e6c2aabee6682
Hanrann6 <yookrann@ewhain.net>
2025-05-26 19:06:17 +0900

    feat: 추천 경우2 추가

--- recommend/recommend_test2.js
+++ recommend/recommend_test2.js
@@ -9,8 +9,36 @@ const client = new recombee.ApiClient(process.env.RECOMBEE_DB, process.env.RECOM
   timeout: 10000,
 });
 
+ //사용자가 도수가 높은 술 위주로 북마크, 상세 페이지 조회를 했다고 가정
+ for (const itemId of [
+  "test-488",
+  "test-487",
+  "test-491",
+  "test-490",
+  "test-494",
+  "test-495",
+  "test-501",
+  "test-466",
+  "test-448",
+  "test-440",
+  "test-344",
+  "test-362",
+]) {
+  await client.send(new rqs.AddBookmark("test2", itemId));
+  await client.send(new rqs.AddDetailView("test2", itemId));
+}
+
 async function recommendByUserPref(userId, count = 5, preference = {}) {
-  const { sweetness, sourness, sparkling, body, abv, type, minPrice, maxPrice } = preference;
+  const {
+    sweetness,
+    sourness,
+    sparkling,
+    body,
+    abv,
+    type,
+    minPrice,
+    maxPrice,
+  } = preference;
 
   // 값이 모두 정수 단위여서 +- 1.0 기준으로 filter 계산
   const filter = `
@@ -18,7 +46,7 @@ async function recommendByUserPref(userId, count = 5, preference = {}) {
     'sourness' >= ${sourness - 1.0} and 'sourness' <= ${sourness + 1.0} and
     'sparkling' >= ${sparkling - 1.0} and 'sparkling' <= ${sparkling + 1.0} and
     'body' >= ${body - 1.0} and 'body' <= ${body + 1.0} and
-    'abv' >= ${abv - 3} and 'abv' <= ${abv + 3} and
+    'abv' >= ${abv - 3} and 'abv' <= ${abv + 10} and
     'type' == "${type}" and
     'price' >= ${minPrice} and 'price' <= ${maxPrice}
   `;
@@ -40,7 +68,7 @@ async function recommendByUserPref(userId, count = 5, preference = {}) {
 // 실행 테스트
 (async () => {
   //임시 유저 가정
-  const userId = "test1";
+  const userId = "test2";
   const userPref = {
     sweetness: 3,
     sourness: 2,
@@ -54,7 +82,7 @@ async function recommendByUserPref(userId, count = 5, preference = {}) {
 
   try {
     const result = await recommendByUserPref(userId, 5, userPref);
-    console.log("추천 결과:");
+    console.log("추천 결과2:");
     result.forEach((item, i) => {
       console.log(`${i + 1}. ${item.name}:  ${item.reason}`);
     });
