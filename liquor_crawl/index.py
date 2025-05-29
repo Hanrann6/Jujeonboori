95aec08a9c43ad9e78570bb8227c9e8137510f53
mia <miasong4678@gmail.com>
2025-05-29 11:09:18 +0900

    feat: csv 정렬 코드 추가

---
+++ liquor_crawl/index.py
@@ -0,0 +1,17 @@
+# merged csv를 가나다순으로 정렬하고 새로 index 부여하는 코드
+
+import pandas as pd
+
+df = pd.read_csv("merged_traditional_alcohol_data.csv", dtype=str)
+
+df = df.drop(columns=[col for col in df.columns if 'Unnamed' in col or col == 'index'])
+
+df = df.sort_values(by="제품명", key=lambda x: x.str.normalize("NFC"))
+
+df = df.reset_index(drop=True)
+df['index'] = df.index
+
+cols = ['index'] + [col for col in df.columns if col != 'index']
+df = df[cols]
+
+df.to_csv("sorted_traditional_alcohol.csv", index=False)
