56e073b86bdfa370a4ee0952f4f85fb07f1867bf
cozy-u <dbrud5804@gmail.com>
2025-08-21 18:23:43 +0900

    [refactor] bookmark 경로 연결

--- Client/Jujeonburi/app/(tabs)/[username]/bookmark.tsx
+++ Client/Jujeonburi/app/(tabs)/[username]/bookmark.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Liked() {
+export default function bookmark() {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
             <Text>찜한 전통주 목록</Text>
