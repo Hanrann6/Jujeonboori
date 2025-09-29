4f29a6fe2b8643c792b7616398b0528a75aa0e82
snowhodut <miasong4678@gmail.com>
2025-09-29 13:27:49 +0900

    [feat] 구글 로그인 일반 OAuth 2.0 방식으로 변경

---
+++ Server/alcohol/model/alcohol.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const alcoholSchema = new mongoose.Schema({
+    name: String,
+    category: String,
+    abv: Number,
+    price: Number,
+    region: String,
+    sweetness: Number,
+    sourness: Number,
+    freshness: Number,
+    body: Number,
+    sparkling: Number,
+    imageUrl: String,
+});
+
+const Alcohol = mongoose.model("Alcohol", alcoholSchema);
+export default Alcohol;
