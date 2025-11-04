b8aa5cc8335bb30c79f087debfd2fca430e63a6a
Hanrann6 <yookrann@ewhain.net>
2025-11-04 13:57:14 +0900

    [Auth] AWS Personalize 추천 token 기반으로 수정

--- Server/routes/recommend.routes.js
+++ Server/routes/recommend.routes.js
@@ -2,11 +2,12 @@ import express from "express";
 import { recommendController } from "../recommend/aws-recommend/controller/recommend.controller.js";
 import priceRecommendRoutes from "./price-recommend.routes.js";
 import weatherRecommendRouter from "./weather-recommend.routes.js";
+import authMiddleware from "../auth/middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.use("/price", priceRecommendRoutes);
 router.use("/weather", weatherRecommendRouter);
-router.get("/:userId", recommendController);
+router.get("/",  authMiddleware.verifyAccessToken, recommendController);
 
 export default router;
